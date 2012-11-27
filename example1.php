c8e92429954417aa247a8f7d32096463d6d46738
ysaotome <y@sotm.jp>
2012-11-27 15:12:06 +0900

    first commit

---
+++ example1.php
@@ -0,0 +1,140 @@
+<?php
+/**
+Facebookサンプルプログラム１
+**/
+
+
+//Facebook公式SDK(開発セット)を読み込む
+//SDKは下記からダウンロードして解凍して設置してください。
+//　https://github.com/facebook/facebook-php-sdk/zipball/v3.2.0
+require './facebook-php-sdk/src/facebook.php';
+
+//AppIDとAppSecretをFacebook Developer Centerにて取得して下さい。
+//　https://developers.facebook.com/apps/
+//AppIDとAppSecretを設定してください。
+$facebook = new Facebook(array(
+    'appId'  => 'APP ID',
+    'secret' => 'APP SECRET',
+));
+
+
+//ログイン状態を取得する
+$user = $facebook->getUser();
+
+if ($user) {
+    try {
+        //ログインしていたら、自分のユーザプロファイルを取得
+        $user_profile = $facebook->api('/me');
+
+    } catch (FacebookApiException $e) {
+        //ユーザプロファイル取得失敗 = ログインしていない
+        error_log($e);
+        $user = null;
+    }
+    
+    try {
+        //ログインしていたら、自分の友達一覧を取得
+        $user_friends = $facebook->api('/me/friends');
+    } catch (FacebookApiException $e) {
+        //友達一覧取得に失敗 = ログインしていない
+        error_log($e);
+        $user = null;
+    } 
+}
+
+if ($user) {
+    //ログインしていたら、ログアウトURLを取得。
+    $params = array( 'next' => 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'] );
+    $logoutUrl = $facebook->getLogoutUrl($params);
+    //セッションのクリア
+    $facebook->destroySession();
+
+} else {
+    //ログインして無いなら、ログインURLを取得。
+    $loginUrl = $facebook->getLoginUrl();
+}
+
+//ユーザの公開情報の取得を試みる（ログインしなくても取得出来る情報）
+//試しに五月女の情報を取得してみる
+$ysaotome = $facebook->api('/ysaotome');
+
+//HTMLヘッダを表示
+echo <<<_HEADER_
+<html>
+    <head>
+        <meta content='text/html; charset=utf-8' http-equiv='content-type'>
+    </head>
+    <p><a href="./">&lt; &lt;サンプル一覧</a></p>
+    <h1>Facebook連携サンプルプログラム１</h1>
+    <p><a href="./example1.php.txt">このページのソースコード</a>(文字コード：UTF-8)</p>
+    <p>出来る事</p>
+    <ul>
+        <li>１）ログイン、ログアウト処理</li>
+        <li>２）ログインしている人の情報を取得する処理</li>
+        <li>３）ログインしている人の友達リストを取得する処理</li>
+        <li>４）ログインしてない状態で取得可能な情報を表示する処理</li>
+    </ul>
+_HEADER_;
+
+//==========================================================================
+echo '<hr />'."\n";
+//ログインボタン、ログアウトボタンを表示
+if ($user) {
+    echo '<a href="'. $logoutUrl .'">ログアウト</a>'."\n";
+} else {
+    echo '<div><a href="'. $loginUrl .'">ログイン</a></div>'."\n";
+}
+
+//==========================================================================
+echo '<hr />'."\n";
+
+//ログインしていたら、ログインしている人の情報を取得する
+if ($user) {
+    echo '<h3>ログインしている人の写真</h3>'."\n";
+    echo '<img src="https://graph.facebook.com/'. $user .'/picture">'."\n";
+
+    echo '<h3>ログインしている人の情報 (/me)</h3>'."\n";
+    echo '<pre>'."\n";
+    echo print_r($user_profile);
+    echo '</pre>'."\n";
+
+    echo '<h3>ログインしている人の友達リスト (/me/friends)</h3>';
+    //友達リストからユーザ情報だけ取得
+    $user_friends_data = $user_friends['data'];
+    echo '<h4>友達の数：'. count($user_friends_data) . ' 人</h4>'."\n";
+    $i=0;
+    foreach ($user_friends_data as $fkey=>$fvalue) {
+        $i++;
+        echo '<a href="http://www.facebook.com/profile.php?id='.$fvalue[id].'"><img src="https://graph.facebook.com/' . $fvalue[id] . '/picture" border="0" title="' . $fvalue[name].'"/></a>';
+        if ($i % 5 == 0) {
+            echo '<br><br>';
+        }
+    }
+
+} else {
+    echo '<strong><em>あなたはまだログインしていません</em></strong>'."\n";
+}
+
+//==========================================================================
+echo '<hr />'."\n";
+
+echo '<h3>公開情報の取得（ログインしなくても取得出来る情報の例）</h3>'."\n";
+echo '<img src="https://graph.facebook.com/ysaotome/picture">'."\n";
+echo $ysaotome['name'];
+
+
+//==========================================================================
+echo '<hr />'."\n";
+
+echo '<h3>デバッグ用の情報</h3>'."\n";
+echo '<pre>'."\n";
+echo print_r($_SESSION);
+echo '</pre>'."\n";
+
+//==========================================================================
+echo '<hr />'."\n";
+
+echo<<<_FOOTER_
+</body>
+</html>
+_FOOTER_;
---
+++ example2.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+Facebookサンプルプログラム２
+**/
+
+//Facebook公式SDK(開発セット)を読み込む
+//SDKは下記からダウンロードして解凍して設置してください。
+//　https://github.com/facebook/facebook-php-sdk/zipball/v3.2.0
+require './facebook-php-sdk/src/facebook.php';
+
+//AppIDとAppSecretをFacebook Developer Centerにて取得して下さい。
+//　https://developers.facebook.com/apps/
+//AppIDとAppSecretを設定してください。
+$facebook = new Facebook(array(
+    'appId'  => 'APP ID',
+    'secret' => 'APP SECRET',
+));
+
+
+//ログイン状態を取得する
+$user = $facebook->getUser();
+
+if ($user) {
+    
+    try {
+        //ログインしていたら、自分のアルバム一覧を取得
+        $user_albums= $facebook->api("/me/albums");
+        //ログインしていたら、自分の所属グループ一覧を取得
+        $user_groups= $facebook->api("/me/groups");
+    } catch (FacebookApiException $e) {
+        //アルバム一覧の取得に失敗 = ログインしていない
+        error_log($e);
+        $user = null;
+    } 
+}
+
+if ($user) {
+    //ログインしていたら、ログアウトURLを取得。
+    $params = array( 'next' => 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'] );
+    $logoutUrl = $facebook->getLogoutUrl($params);
+    //セッションのクリア
+    $facebook->destroySession();
+
+} else {
+    //ログインして無いなら、ログインURLを取得。
+    $params = array(
+      //【大事】ログイン時に同時に取得する権限。下記は「写真」と「グループ」情報へのアクセス許可を取得
+      //詳細は　https://developers.facebook.com/docs/authentication/permissions/　参照
+      'scope' => 'user_photos, user_groups',
+      'redirect_uri' => 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'] ,
+    );
+    $loginUrl = $facebook->getLoginUrl($params);
+}
+
+//HTMLヘッダを表示
+echo <<<_HEADER_
+<html>
+    <head>
+        <meta content='text/html; charset=utf-8' http-equiv='content-type'>
+    </head>
+    <p><a href="./">&lt; &lt;サンプル一覧</a></p>
+    <h1>Facebook連携サンプルプログラム２</h1>
+    <p><a href="./example2.php.txt">このページのソースコード</a>(文字コード：UTF-8)</p>
+    <p>出来る事</p>
+    <ul>
+        <li>１）ログインユーザの投稿アルバム（写真）の一覧を取得</li>
+        <li>２）ログインユーザの所属グループの一覧を取得</li>
+        <li>その他参考になりそうなページ
+        <ul>
+        <li><a href="https://developers.facebook.com/docs/reference/api/">Facebookでユーザのどんな情報にアクセス出来るか一覧</a></li>
+　      <li><a href="https://developers.facebook.com/docs/authentication/permissions/">Facebookでユーザの持っている情報へアクセスする時の許可リスト</a></li>
+        </ul>
+        </li>
+    </ul>
+_HEADER_;
+
+//==========================================================================
+echo '<hr />'."\n";
+//ログインボタン、ログアウトボタンを表示
+if ($user) {
+    echo '<a href="'. $logoutUrl .'">ログアウト</a>'."\n";
+} else {
+    echo '<div><a href="'. $loginUrl .'">ログイン</a></div>'."\n";
+}
+
+//==========================================================================
+echo '<hr />'."\n";
+
+//ログインしていたら、取得した情報を表示する
+if ($user) {
+    echo '<h3>ログインしている人の写真</h3>'."\n";
+    echo '<img src="https://graph.facebook.com/'. $user .'/picture">'."\n";
+
+    echo '<h3>ログインしている人のアルバム一覧 (/me/albums)</h3>';
+    echo '<pre>'."\n";
+    echo print_r($user_albums);
+    echo '</pre>'."\n"; 
+  
+    //個々のアルバム情報を表示
+    echo '<h3>ログインしている人のアルバムへのリンク</h3>';
+    foreach ($user_albums['data'] as $key_album => $val_album) {
+      echo '<h4><a href="'.$val_album['link'].'">'.$val_album['name'].'</a></h4>'."\n";
+    }
+
+    //==========================================================================
+    echo '<hr />'."\n";
+
+    echo '<h3>ログインしている人の所属グループ一覧 (/me/groups)</h3>';
+    echo '<pre>'."\n";
+    echo print_r($user_groups);
+    echo '</pre>'."\n"; 
+  
+    //個々のグループ情報を表示
+    foreach ($user_groups['data'] as $key_group => $val_group) {
+        echo '<h4><a href="https://www.facebook.com/groups/'.$val_group['id'].'">'.$val_group['name'].'</a></h4>'."\n";
+    }
+
+    //==========================================================================
+    echo '<hr />'."\n";
+ 
+  
+} else {
+    echo '<strong><em>あなたはまだログインしていません</em></strong>'."\n";
+}
+
+
+//==========================================================================
+echo '<hr />'."\n";
+
+echo '<h3>デバッグ用の情報</h3>'."\n";
+echo '<pre>'."\n";
+echo print_r($_SESSION);
+echo '</pre>'."\n";
+
+//==========================================================================
+echo '<hr />'."\n";
+
+echo<<<_FOOTER_
+</body>
+</html>
+_FOOTER_;
